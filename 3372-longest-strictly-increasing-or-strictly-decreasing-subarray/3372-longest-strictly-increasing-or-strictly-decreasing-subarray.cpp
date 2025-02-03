6e453adac12b014f0c88aa10b6019827fdda3604
Youssef Ahmed <102183159+0YoussefAhmed00@users.noreply.github.com>
2025-02-03 23:46:49 +0200

    Time: 0 ms (100%), Space: 27.9 MB (27.25%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,14 +1,8 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-        int count1 = 1;
-        int count2 = 1;
-        int maxLength = 1;
-
-        for (int i = 0; i < nums.size() - 1; i++) {
+        int count1 = 1, count2 = 1, maxCount = 1;
+        for (int i = 0; i < nums.size() - 1; ++i) {
             if (nums[i] < nums[i + 1]) {
                 count1++;
                 count2 = 1;
@@ -16,12 +10,12 @@ public:
                 count2++;
                 count1 = 1;
             } else {
+                maxCount = max(maxCount, max(count1, count2));
                 count1 = 1;
                 count2 = 1;
             }
-            maxLength = max(maxLength, max(count1, count2));
+            maxCount = max(maxCount, max(count1, count2));
         }
-
-        return maxLength;
+        return maxCount;
     }
 };
