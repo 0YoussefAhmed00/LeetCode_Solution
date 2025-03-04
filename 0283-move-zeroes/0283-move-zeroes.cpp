6accba345e7e6049517334b98b67b43809888279
Youssef Ahmed <102183159+0YoussefAhmed00@users.noreply.github.com>
2025-03-05 00:16:30 +0200

    Time: 4 ms (14.56%), Space: 23.8 MB (50.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){ 
-            nums.erase(nums.begin()+i);
-           count++;
-           i--;
+        int zzero = 0; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[zzero]);
+                zzero++; 
             }
-        }
-        for(int i=0;i<count;i++){
-            nums.push_back(0);
         }
     }
 };
